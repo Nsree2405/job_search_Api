6d6140844891b0e77c239215d77db62c2b673262
Nsree <navyasree2405@gmail.com>
2025-02-12 20:23:52 +0530

    success

--- app.py
+++ app.py
@@ -16,12 +16,8 @@
 #             print(final)
 #     return render_template('home.html')
 
-
-
 # if __name__ == '__main__':
 #     app.run(debug=True)  
-
-
 
 from flask import Flask, render_template, request, redirect
 import jobs
